9844e988800d46e22b46c4c046f3d710b4c31e15
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-05-05 00:37:31 -0300

    updating bundler

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 Offert.create(description: 'Notebook Lenovo Core i5-8265U 8GB 1TB Tela 15.6 Windows 10 Ideapad S145.', value: 2659)
+
 Offert.create(description: 'Notebook Lenovo Core i5-8265U 8GB 1TB Tela 15.6 sem o sistema operacional.', value: 2000)
+
 Offert.create(description: 'Notebook Lenovo Core i5-8265U 8GB 1TB Tela 15.6 com Linux.', value: 2200)
+
 Offert.create(description: 'Notebook Lenovo Core i5-8265U 8GB 1TB Tela 15.6 sem mouse e sistema operacional.', value: 1900)
